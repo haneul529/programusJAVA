4ce1273ec1e48a0b7a37c77b670eac385748b8a4
haneul529 <lucia00529@skuniv.ac.kr>
2024-04-07 15:22:21 +0900

    강의 듣고 실습

---
+++ IfExample.java
@@ -0,0 +1,21 @@
+package programmus.beginner;
+
+public class IfExample {
+
+	public static void main(String[] args) {
+		int x = 50;
+		int y = 60;
+		
+		if(x<y) {
+			System.out.println("x는 y보다 작습니다.");
+		}
+		
+		if(x==y) {
+			System.out.println("x는 y와 같습니다.");
+		} else {
+			System.out.println("x와 y는 다릅니다.");
+		}
+		
+	}
+
+}
